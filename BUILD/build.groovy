0a4596f4ba23ce9aa89e653744df9a73ddfeed03
Pavel Ushakov <ushakovpb@gmail.com>
2023-01-15 11:25:27 +0400

    Changed build.groovy

--- BUILD/build.groovy
+++ BUILD/build.groovy
@@ -1,10 +1,15 @@
-pipeline {
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                    sh 'gradle test -Djavax.net.ssl.trustStore=$JAVA_HOME/lib/security/cacerts -Djavax.net.ssl.trustStorePassword=changeit'
-            }
-        }
+//pipeline {
+//    agent any
+//    stages {
+//        stage('Build') {
+//            steps {
+//                sh 'gradle test -Djavax.net.ssl.trustStore=$JAVA_HOME/lib/security/cacerts -Djavax.net.ssl.trustStorePassword=changeit'
+//            }
+//        }
+//    }
+//}
+node {
+    stage('Build') {
+        sh 'gradle test -Djavax.net.ssl.trustStore=$JAVA_HOME/lib/security/cacerts -Djavax.net.ssl.trustStorePassword=changeit'
     }
 }
