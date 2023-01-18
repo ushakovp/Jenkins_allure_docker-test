149a1e9c941c26fec6087fb4bfa9339d3b831a21
Pavel Ushakov <ushakovpb@gmail.com>
2023-01-18 20:40:20 +0400

    Changed build.groovy

--- src/test/java/test/GithubTest.java
+++ src/test/java/test/GithubTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -14,6 +15,7 @@ public class GithubTest {
 
     @Test
     void githubSearch(){
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         step("Открываем главную страницу", () -> {
