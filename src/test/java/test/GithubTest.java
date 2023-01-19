5b2091fa548dde8f4e55cb2563ce1cec27db1ef6
Pavel Ushakov <ushakovpb@gmail.com>
2023-01-19 21:39:49 +0400

    Changed build.groovy

--- src/test/java/test/GithubTest.java
+++ src/test/java/test/GithubTest.java
@@ -6,6 +6,12 @@ import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -14,10 +20,16 @@ public class GithubTest {
     public final SelenideElement githubSearch = $("input[name=q]");
 
     @Test
-    void githubSearch(){
+    void githubSearch() throws MalformedURLException {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
-
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.browser = System.getProperty("browser_name", "firefox");
+        Configuration.browserVersion = System.getProperty("browser_version", "98.0");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
         step("Открываем главную страницу", () -> {
             open("https://github.com");
         });
