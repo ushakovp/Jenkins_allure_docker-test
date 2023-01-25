6a44c2a649b2e58c47c18d73b123f4a1428054f7
Pavel Ushakov <ushakovpb@gmail.com>
2023-01-25 22:08:33 +0400

    Changed build.groovy

--- BUILD/build.groovy
+++ BUILD/build.groovy
@@ -14,12 +14,12 @@ node {
         sh 'gradle test -g gradle-user-home -Djavax.net.ssl.trustStore=$JAVA_HOME/lib/security/cacerts -Djavax.net.ssl.trustStorePassword=changeit'
     }
     stage('Reports') {
-            allure([
-                    includeProperties: false,
-                    jdk: '',
-                    properties: [],
-                    reportBuildPolicy: 'ALWAYS',
-                    results: [[path: 'build//allure-results']]
-            ])
+        allure([
+                includeProperties: false,
+                jdk: '',
+                properties: [],
+                reportBuildPolicy: 'ALWAYS',
+                results: [[path: 'build/allure-results']]
+        ])
     }
 }
