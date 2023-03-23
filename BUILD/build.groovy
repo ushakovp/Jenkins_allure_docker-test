a13250d36ef3ec49559e10250ff70aee05dfd9af
Pavel Ushakov <ushakovpb@gmail.com>
2023-03-23 21:58:37 +0400

    another build.groovy

--- BUILD/build.groovy
+++ BUILD/build.groovy
@@ -4,8 +4,7 @@ node {
         sh "docker build -t java-autotests -f Dockerfile ."
     }
     stage('Run Tests') {
-        sh "docker run --rm -u jenkins:jenkins -v ${WORKSPACE}/allure-results:/app/build/allure-results java-autotests gradle test"
-        sh "chown -R jenkins:jenkins allure-results"
+            sh 'docker run --rm -u $(id -u jenkins):$(id -g jenkins) -v ${WORKSPACE}/allure-results:/app/build/allure-results -e HOME=/home/jenkins -e USER=jenkins -w /home/jenkins java-autotests bash -c "groupadd -g $(id -g jenkins) jenkins && useradd -u $(id -u jenkins) -g $(id -g jenkins) -d /home/jenkins -m -s /bin/bash jenkins && chown -R jenkins:jenkins /app/build/allure-results && su jenkins -c \"gradle test\""'
     }
     stage('Reports') {
         allure([
