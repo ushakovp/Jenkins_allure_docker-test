36307db20c86a9d4871a9354363089f8fdf04f6f
Pavel Ushakov <ushakovpb@gmail.com>
2023-03-25 16:47:33 +0400

    another build.groovy

--- BUILD/build.groovy
+++ BUILD/build.groovy
@@ -1,10 +1,16 @@
 node {
     checkout scm
+    def containerId
     stage('Build Docker Image') {
         sh "docker build -t java-autotests -f Dockerfile ."
     }
     stage('Run Tests') {
-            sh "docker run --rm -v ${WORKSPACE}/allure-results:/app/build/allure-results java-autotests gradle test"
+        containerId = sh(script: 'docker run -d java-autotests', returnStdout: true).trim()
+        sh "docker exec ${containerId} gradle test"
+    }
+    stage('Copy Allure Results') {
+        sh "docker cp ${containerId}:/app/build/allure-results ${WORKSPACE}/allure-results"
+        sh "docker rm -f ${containerId}"
     }
     stage('Reports') {
         allure([
